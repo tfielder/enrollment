531c58ebbaae4f4c54337c14b4e838f198005158
Tim Fielder <fiel1139@gmail.com>
2018-08-28 15:16:20 -0600

    links student names.

--- spec/features/user_sees_students_spec.rb
+++ spec/features/user_sees_students_spec.rb
@@ -11,5 +11,15 @@ describe "As a user" do
       expect(page).to have_content(s1.name)
       expect(page).to have_content(s2.name)
     end
+    it "shows links to students" do
+      s1 = Student.create(name: "Turing")
+      s2 = Student.create(name: "Kelly")
+
+      visit students_path
+
+      click_link "Turing"
+
+      expect(current_path).to eq(student_path(s1))
+    end
   end
 end
