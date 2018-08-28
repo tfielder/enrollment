bfc602c27e7add6fb0386befee289151cd077d35
Tim Fielder <fiel1139@gmail.com>
2018-08-28 15:47:33 -0600

    fixes db.

--- db/migrate/20180828212838_add_student_to_address.rb
+++ db/migrate/20180828212838_add_student_to_address.rb
@@ -1,4 +1,4 @@
-class AddStudentToAddresses < ActiveRecord::Migration[5.1]
+class AddStudentToAddress < ActiveRecord::Migration[5.1]
   def change
     add_reference :addresses, :student, index: true, foreign_key: true
   end
